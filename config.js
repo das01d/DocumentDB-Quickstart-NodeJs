6496b6f3e34a4713dc794b9e06d73bf153dc42a9
Dennis Somerville <78747459+das01d@users.noreply.github.com>
2022-04-04 22:27:12 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,97 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+var dotenvConfig = dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+})
+
+var config = {}
+
+config.endpoint = process.env.ENDPOINT
+config.key = process.env.KEY
+
+config.database = {
+  // id: 'db'
+  id: process.env.DATABASE
+}
+
+config.container = {
+  id: 'items'
+}
+
+config.items = {
+  Andersen: {
+    id: 'Anderson.1',
+    Country: 'USA',
+    lastName: 'Andersen',
+    parents: [
+      {
+        firstName: 'Thomas'
+      },
+      {
+        firstName: 'Mary Kay'
+      }
+    ],
+    children: [
+      {
+        firstName: 'Henriette Thaulow',
+        gender: 'female',
+        grade: 5,
+        pets: [
+          {
+            givenName: 'Fluffy'
+          }
+        ]
+      }
+    ],
+    address: {
+      state: 'WA',
+      county: 'King',
+      city: 'Seattle'
+    }
+  },
+  Wakefield: {
+    id: 'Wakefield.7',
+    Country: 'Italy',
+    parents: [
+      {
+        familyName: 'Wakefield',
+        firstName: 'Robin'
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Ben'
+      }
+    ],
+    children: [
+      {
+        familyName: 'Merriam',
+        firstName: 'Jesse',
+        gender: 'female',
+        grade: 8,
+        pets: [
+          {
+            givenName: 'Goofy'
+          },
+          {
+            givenName: 'Shadow'
+          }
+        ]
+      },
+      {
+        familyName: 'Miller',
+        firstName: 'Lisa',
+        gender: 'female',
+        grade: 1
+      }
+    ],
+    address: {
+      state: 'NY',
+      county: 'Manhattan',
+      city: 'NY'
+    },
+    isRegistered: false
+  }
+}
+
+module.exports = config
